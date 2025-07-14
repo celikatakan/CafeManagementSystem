7eca73a3b87ea96342323540eda464e5d09292bd
Atakan <143435515+celikatakan@users.noreply.github.com>
2025-07-14 09:48:37 +0300

    obj ve bin dosyaları hariç tüm değişiklikler için rebase öncesi commit

--- CafeManagementSystem.Data/obj/Debug/net7.0/CafeManagementSystem.Data.AssemblyInfo.cs
+++ CafeManagementSystem.Data/obj/Debug/net7.0/CafeManagementSystem.Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CafeManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc6784ab3d121fff55344fc36e648f006a32b5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("CafeManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CafeManagementSystem.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
